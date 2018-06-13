501014cd3877eaa5e9934c1b1b19704808c70f23
Fubiven <1187958043@qq.com>
2018-06-13 15:14:07 +0800

    first commit

---
+++ MyKotlinAndroid/app/src/main/java/com/example/fzp/mykotlinandroid/forJava/Chapter4JavaActivity.java
@@ -0,0 +1,26 @@
+package com.example.fzp.mykotlinandroid.forJava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fzp.mykotlinandroid.R;
+
+/**
+ * 类、对象和接口（Java）
+ * Created by Fuzp on 2018/06/12
+ */
+public class Chapter4JavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter4);
+
+        initView();
+    }
+
+
+    private void initView() {
+
+    }
+}
---
+++ MyKotlinAndroid/app/src/main/java/com/example/fzp/mykotlinandroid/forKotlin/Chapter4Activity.kt
@@ -0,0 +1,17 @@
+package com.example.fzp.mykotlinandroid.forKotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fzp.mykotlinandroid.R
+
+/**
+ * 类、对象和接口(Kotlin)
+ * Created by Fuzp on 2018/06/12
+ */
+class Chapter4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter4)
+    }
+}
